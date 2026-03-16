319e55e996fb48fd8f71a061a707b93bf0d4763b
davidtahimalvesbrito <davidtahim@gmail.com>
2026-03-16 19:38:50 -0300

    adiciona instância de Filme e exibe ficha técnica no método main

--- Principal.java
+++ Principal.java
@@ -1,5 +1,10 @@
 public class Principal {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        Filme filme1 = new Filme();
+            filme1.nome = "O Poderoso Chefão";
+            filme1.anoDeLancamento = 1972;
+            filme1.totalEmMinutos = 175;
+
+        filme1.exibeFichaTecnica();
     }
 }
