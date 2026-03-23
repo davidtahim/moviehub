a0447603d4298da8ac44cb82d035c3550222aeb9
David Tahim <davidtahim@gmail.com>
2026-03-23 14:21:25 -0300

    refatora classe Filme para encapsulamento e atualiza uso de métodos na classe Principal

--- br/com/cdl/moviehub/modelos/Filme.java
+++ br/com/cdl/moviehub/modelos/Filme.java
@@ -1,36 +1,49 @@
 package br.com.cdl.moviehub.modelos;
 
 public class Filme {
-    
-    //declarando os atributos da classe
-    String nome;
-    int anoDeLancamento, totalEmMinutos, totalDeAvaliacoes;
-    double somaDasAvaliacoes,nota, pegaMedia;
 
-    //criando as ações da classe
+    // atributos privados (encapsulamento)
+    private String nome;
+    private int anoDeLancamento;
+    private int totalEmMinutos;
+    private int totalDeAvaliacoes;
+    private double somaDasAvaliacoes;
+    private boolean incluidoNoPlano;
 
-    void exibeFichaTecnica(){
-        System.out.println("Nome do filme: " + nome);
-        System.out.println("Ano de lançamento: " + anoDeLancamento);
-        System.out.println("Duração em minutos: " + totalEmMinutos);
+    // ações
+    public void exibeFichaTecnica(){
+        System.out.println("Nome do filme: " + this.nome);
+        System.out.println("Ano de lançamento: " + this.anoDeLancamento);
+        System.out.println("Duração em minutos: " + this.totalEmMinutos);
     }
 
-    void avalia(double nota){
-
-        somaDasAvaliacoes += nota;
-        totalDeAvaliacoes++;
-
+    public void avalia(double nota){
+        this.somaDasAvaliacoes += nota;
+        this.totalDeAvaliacoes++;
     }
 
-    double pegaMedia(){
-
-        return somaDasAvaliacoes / totalDeAvaliacoes;
-
+    // método que calcula a média (permanece público)
+    public double pegaMedia(){
+        if (this.totalDeAvaliacoes == 0) return 0;
+        return this.somaDasAvaliacoes / this.totalDeAvaliacoes;
     }
 
+    // getters e setters públicos
+    public String getNome() { return this.nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
+    public int getAnoDeLancamento() { return this.anoDeLancamento; }
+    public void setAnoDeLancamento(int ano) { if (ano > 1888) this.anoDeLancamento = ano; }
 
+    public int getTotalEmMinutos() { return this.totalEmMinutos; }
+    public void setTotalEmMinutos(int minutos) { if (minutos > 0) this.totalEmMinutos = minutos; }
 
+    public int getTotalDeAvaliacoes() { return this.totalDeAvaliacoes; }
+    // não expor setTotalDeAvaliacoes publicamente normalmente
 
+    public double getSomaDasAvaliacoes() { return this.somaDasAvaliacoes; }
+
+    public boolean isIncluidoNoPlano() { return this.incluidoNoPlano; }
+    public void setIncluidoNoPlano(boolean incluido) { this.incluidoNoPlano = incluido; }
 
 }
