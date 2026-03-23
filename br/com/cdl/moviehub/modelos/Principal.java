403590ab2924122c77c4dc2e8440b57af94fd861
David Tahim <davidtahim@gmail.com>
2026-03-23 14:26:56 -0300

    adiciona interação para entrada de dados do filme e avaliações na classe Principal

--- br/com/cdl/moviehub/modelos/Principal.java
+++ br/com/cdl/moviehub/modelos/Principal.java
@@ -1,20 +1,38 @@
 package br.com.cdl.moviehub.modelos;
+
+import java.util.Scanner;
+
 public class Principal {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         Filme filme1 = new Filme();
-            filme1.setNome("O Poderoso Chefão");
-            filme1.setAnoDeLancamento(1972);
-            filme1.setTotalEmMinutos(175);
 
+        System.out.print("Nome do filme: ");
+        String nome = scanner.nextLine();
+        filme1.setNome(nome);
+
+        System.out.print("Ano de lançamento: ");
+        int ano = scanner.nextInt();
+        filme1.setAnoDeLancamento(ano);
+
+        System.out.print("Duração em minutos: ");
+        int minutos = scanner.nextInt();
+        filme1.setTotalEmMinutos(minutos);
+
+        System.out.print("Quantas avaliações você quer inserir? ");
+        int qtd = scanner.nextInt();
+        for (int i = 0; i < qtd; i++) {
+            System.out.print("Avaliação #" + (i + 1) + ": ");
+            double nota = scanner.nextDouble();
+            filme1.avalia(nota);
+        }
+
+        System.out.println();
         filme1.exibeFichaTecnica();
-        //Lançando as avaliações do filme
-        filme1.avalia(8);
-        filme1.avalia(9.5);
-        filme1.avalia(10);
-
-        //Exibindo as avaliacoes do filme
         System.out.println("Soma das avaliações: " + filme1.getSomaDasAvaliacoes());
         System.out.println("Total de avaliações: " + filme1.getTotalDeAvaliacoes());
         System.out.println("Média das avaliações: " + filme1.pegaMedia());
+
+        scanner.close();
     }
 }
