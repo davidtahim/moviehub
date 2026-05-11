60f2e3284feb736d95ead46bd8420e8df5c1dad6
David Tahim <davidtahim@gmail.com>
2026-05-11 19:37:32 -0300

    remove métodos de cálculo de tempo para Filme e Série e atualiza método inclui para usar Titulo

--- src/br/com/cdl/moviehub/calculos/CalculadoraDeTempo.java
+++ src/br/com/cdl/moviehub/calculos/CalculadoraDeTempo.java
@@ -1,7 +1,6 @@
 package br.com.cdl.moviehub.calculos;
 
-import br.com.cdl.moviehub.modelos.Filme;
-import br.com.cdl.moviehub.modelos.Serie;
+import br.com.cdl.moviehub.modelos.Titulo;
 
 public class CalculadoraDeTempo {
     private int tempoTotal = 0;
@@ -10,14 +9,20 @@ public class CalculadoraDeTempo {
         return tempoTotal;
     }
 
-    //método para calcular o tempo de um filme
-    public void inclui(Filme f) {
-        this.tempoTotal += f.getTotalEmMinutos();
-    }
+     //método para calcular o tempo de um filme
+//    public void inclui(Filme f) {
+//        this.tempoTotal += f.getTotalEmMinutos();
+//    }
 
     //método para calcular o tempo de uma série
-    public void inclui(Serie s) {
-        this.tempoTotal += s.getTemporadas() * s.getEpisodiosPorTemporada() * s.getMinutosPorEpisodio();
-    }
+//    public void inclui(Serie s) {
+//        this.tempoTotal += s.getTemporadas() * s.getEpisodiosPorTemporada() * s.getMinutosPorEpisodio();
+//    }
+
+public void inclui(Titulo titulo) {
+    System.out.println("Adicionando duracao em minutos de " + titulo);
+    this.tempoTotal += titulo.getDuracaoEmMinutos();
+}
+
 
 }
